37938aa8e64bdb5610c4cc75c8d66c9f146c1c40
hapcosa <hapcosa@gmail.com>
2025-07-15 22:10:17 -0400

    Versión inicial de la app Node.jsbuild pipeline

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = 3000;
+const requestHandler = (req, res) => {
+    res.end('🚀 Aplicación desplegada automáticamente con AWS Pipeline');
+};
+const server = http.createServer(requestHandler);
+server.listen(port, '0.0.0.0',() => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
